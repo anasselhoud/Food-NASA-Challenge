d15bbca0414134737ade81628a09a7847167e42b
Anass El Houd <anass.elhoud@gmail.com>
2020-06-01 01:56:56 +0200

    Update demand-forecasting.py

--- demand-forecasting.py
+++ demand-forecasting.py
@@ -141,3 +141,102 @@ train_predict = inverse_normalize_data(train_predict, scal)
 y_train = inverse_normalize_data(y_train, scaler)
 validate_predict = inverse_normalize_data(val_predict, scal)
 y_validate = inverse_normalize_data(y_validate, scal)
+
+
+
+from random import randint
+from numpy import array
+from numpy import argmax
+from numpy import array_equal
+from keras.utils import to_categorical
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.layers import Dense
+
+from keras import backend as K
+from keras.models import Sequential, load_model, Model
+from keras.layers import LSTM, Dense, Activation, TimeDistributed, Dropout, Lambda, RepeatVector, Input, Reshape
+from keras.callbacks import ModelCheckpoint
+
+    seq_length = time_step + after_day
+    result = []
+    for index in range(len(data) - seq_length + 1):
+        result.append(data[index: index + seq_length])
+    result = np.array(result)
+    print('total data: ', result.shape)
+
+    train_size = int(len(result) * validate_percent)
+    train = result[:train_size, :]
+    validate = result[train_size:, :]
+
+    x_train = train[:, :time_step]
+    y_train = train[:, time_step:]
+    x_validate = validate[:, :time_step]
+    y_validate = validate[:, time_step:]
+
+    return [x_train, y_train, x_validate, y_validate]
+
+
+def base_model(feature_len=3, after_day=3, input_shape=(8, 1)):
+    model = Sequential()
+
+    model.add(LSTM(units=100, return_sequences=False, input_shape=input_shape))
+
+    model.add(RepeatVector(after_day))
+    model.add(LSTM(200, return_sequences=True))
+
+    model.add(TimeDistributed(Dense(units=feature_len, activation='linear')))
+
+    return model
+
+
+def seq2seq(feature_len=1, after_day=1, input_shape=(8, 1)):
+    '''
+    Encoder:
+    X = Input sequence
+    C = LSTM(X); The context vector
+
+    Decoder:
+    y(t) = LSTM(s(t-1), y(t-1)); where s is the hidden state of the LSTM(h and c)
+    y(0) = LSTM(s0, C); C is the context vector from the encoder.
+    '''
+
+    # Encoder
+    encoder_inputs = Input(shape=input_shape) 
+    encoder = LSTM(units=100, return_state=True,  name='encoder')
+    encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+    states = [state_h, state_c]
+
+    # Decoder
+    reshapor = Reshape((1, 100), name='reshapor')
+    decoder = LSTM(units=100, return_sequences=True, return_state=True, name='decoder')
+
+    # Densor
+    densor_output = Dense(units=feature_len, activation='linear', name='output')
+
+    inputs = reshapor(encoder_outputs)
+    #inputs = tdensor(inputs)
+    all_outputs = []
+
+
+    for _ in range(after_day):
+        outputs, h, c = decoder(inputs, initial_state=states)
+
+        inputs = outputs
+        states = [state_h, state_c]
+
+        outputs = densor_output(outputs)
+        all_outputs.append(outputs)
+
+    decoder_outputs = Lambda(lambda x: K.concatenate(x, axis=1))(all_outputs)
+    model = Model(inputs=encoder_inputs, outputs=decoder_outputs)
+
+    return model
+
+
+
+input_shape = (3, data.shape[1])
+model = seq2seq(data.shape[1], 4, input_shape)
+model.compile(loss='mse', optimizer='adam',metrics=['acc'])
+model.summary()
